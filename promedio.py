68ade88ea97bfb87333423bafdd19b8d80ff42f4
Angytap82 <angytap82@gmail.com>
2026-06-23 22:55:25 -0300

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,5 @@
+print("--- Sistema de Notas (AYSO) ---")
+nota1 = float(input("Ingrese la primer nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+promedio = (nota1 + nota2) / 2
+print("El promedio final es:", promedio)
